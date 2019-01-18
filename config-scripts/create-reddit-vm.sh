b9ab52ec5fd0d9ff9c831ba06c118591f3edd141
Victoria <victoria.uskova@gmail.com>
2019-01-18 22:31:26 +0300

    HW done 7 (2)

---
+++ config-scripts/create-reddit-vm.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+#
+gcloud compute instances create reddit-app\
+ --boot-disk-size=10GB \
+ --image-family reddit-full \
+ --machine-type=g1-small \
+ --tags puma-server \
+ --restart-on-failure
